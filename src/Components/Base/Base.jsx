44ced7bddf9a83b1eae982f6843cef728c905ebe
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-01-13 17:22:57 +0530

    some work done

--- src/Components/Base/Base.jsx
+++ src/Components/Base/Base.jsx
@@ -1,15 +1,20 @@
+import { useEffect, useState } from 'react';
+
 const Base = () => {
+
+    const [close, setClose] = useState(false);
+
     return (
-        <div className='flex min-h-[650px] ml-10 mr-10 border-b-2 border-gray-500'>
-            <div className='w-2/5 absolute left-0 bottom-[4%] text-left'>
-                <h2 className="text-[38px] pl-10 font-semibold leading-tight">
+        <div className={close ? `flex min-h-[650px] ml-10 mr-10 border-b-2 border-gray-500` : `flex flex-col h-[45vh] ml-6 mr-6 border-b-2 border-gray-500`}>
+            <div className={close ? `w-2/5 absolute left-0 bottom-[4%] text-left`:`w-[85%] absolute left-0 bottom-[48%] text-left`}>
+                <h2 className={close ? `text-[38px] pl-10 font-semibold leading-tight`:`text-[24px] w-full pl-10 font-semibold leading-[1]`}>
                     sundown is a multi-disciplinary studio focused on creating unique, end to end experience and eniviroment.
                 </h2>
             </div>
-            <div className='w-3/5 absolute right-0 bottom-20 pr-10 font-semibold'>
-                <h1 className='text-right text-[10vw] leading-[.9] tracking-normal bold uppercase'>Spaces</h1>
-                <h1 className='text-right text-[10vw] leading-[.9] tracking-normal bold uppercase'>that</h1>
-                <h1 className='text-right text-[10vw] leading-[.9] tracking-normal bold uppercase'>inspire</h1>
+            <div className={close ? `w-3/5 absolute right-0 bottom-20 pr-10 font-semibold` : `w-4/5 absolute right-0 top-32 pr-4 font-semibold`}>
+                <h1 className={close ? `text-right text-[10vw] leading-[.9] tracking-normal bold uppercase` : 'text-right text-[15vw] leading-[.9] tracking-normal bold uppercase'}>Spaces</h1>
+                <h1 className={close ? `text-right text-[10vw] leading-[.9] tracking-normal bold uppercase` : 'text-right text-[15vw] leading-[.9] tracking-normal bold uppercase'}>that</h1>
+                <h1 className={close ? `text-right text-[10vw] leading-[.9] tracking-normal bold uppercase` : 'text-right text-[15vw] leading-[.9] tracking-normal bold uppercase'}>inspire</h1>
             </div>
         </div>
     )
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,21 +1,36 @@
+import { useEffect, useState } from 'react';
 import './Nav.css';
 
 const Nav = () => {
+
+  const [close, setClose] = useState(false);
+
+  // useEffect(() => {
+  //   const handleClose = () => {
+  //     if(window.screen.width <= "768px"){
+  //       setClose(false);
+  //     }
+  //   }
+  // },[close, setClose,]);
+
   return (
     <div className='bg-[#EFEAE3] relative z-10 flex justify-between p-5'>
         <div className='w-1/5'>
             <img src="https://assets-global.website-files.com/64d3dd9edfb41666c35b15b7/64d3dd9edfb41666c35b15c2_Sundown%20logo.svg" alt="" />
         </div>
-        <div className='w-[28%] flex items-center justify-evenly text-2xl'>
+        {close ? <div className='w-[28%] flex items-center justify-evenly text-2xl'>
             <h2 className=' w-[25%] text-center border-2 border-black rounded-[50px] p-1 relative overflow-hidden butt'><a href="#">Work</a></h2>
             <h2 className=' w-[25%] text-center border-2 border-black rounded-[50px] p-1 relative overflow-hidden butt'><a href="#">Studio</a></h2>
             <h2 className=' w-[25%] text-center border-2 border-black rounded-[50px] p-1 relative overflow-hidden butt'><a href="#">Contact</a></h2>
-        </div>
+        </div> : <div className='w-[35%] text-2xl border-2 border-black rounded-[50px] flex'>
+            <div className='w-[30%] flex flex-col items-center justify-center pl-2'>
+              <div className='h-[8%] w-[70%] mb-1 bg-black rounded-md'></div>
+              <div className='h-[8%] w-[70%] bg-black rounded-md'></div>
+            </div>
+            <h2 className=' w-[70%] text-center p-1 overflow-hidden'>Menu</h2>
+        </div>}
     </div>
   )
 }
 
 export default Nav
-
-
-// eniviroment exp content 
