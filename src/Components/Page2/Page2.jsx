4cb0f41d5429203e0bb00c2d7c7eb998d73d38c3
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-01-13 20:47:52 +0530

    one more

--- src/Components/Page2/Page2.jsx
+++ src/Components/Page2/Page2.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import './Page2.css';
 const Page2 = () => {
 
+    const [close, setClose] = useState(false);
+
     return (
-        <div className="min-h-[100vh] w-full -mt-32 relative fixe">
+        <div className={close ? "min-h-[100vh] w-full -mt-32 relative fixe" : "min-h-[100vh] w-full mt-16 relative fixe"}>
             <div>
-                {title.map((title, index) => (
+                {close ? (title.map((title, index) => (
                     <div key={index} className="border-b-2 border-gray-300 relative overflow-hidden hell h-[8vw] flex items-center justify-between pr-10"
                         >
                         <div className="text-5xl p-4 pl-10 relative z-10 font-bold uppercase">
@@ -15,7 +18,19 @@ const Page2 = () => {
                             <h5 className="text-sm font-thin text-gray-500 capitalize">{desc2[index]}</h5>
                         </div>
                     </div>
-                ))}
+                ))) : (title.map((title, index) => (
+                    <div key={index} className='w-[90%] h-[60vh] mr-auto ml-auto mb-8 rounded-t-3xl'>
+                        <div className='w-full h-[79%] bg-fuchsia-300 rounded-3xl'>
+                            <img src="#" alt="img" className='w-full h-full object-cover' />
+                        </div>
+                        <div >
+                            <h1 className="text-3xl mt-6 font-bold uppercase">{title}</h1>
+                            <h1 className="text-md mt-2 uppercase">{desc[index]}</h1>
+                            <h5 className="text-sm font-thin text-gray-500 capitalize">{desc2[index]}</h5>
+                        </div>
+
+                    </div>
+                )))}
             </div>
         </div>
     )
