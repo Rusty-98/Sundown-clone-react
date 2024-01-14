94dece4d741da7b35add28af22489c7e7c1d18f2
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-01-14 09:59:54 +0530

    done bhai

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,10 +1,12 @@
 // Loader.js
 import { useEffect, useState } from "react";
 import './Loader.css'; // Import your CSS file
+import { useResponse } from "../../Hooks/useResponse";
 
 const Loader = () => {
   const [cont, setCont] = useState("Environment");
   const [load, setLoad] = useState(true);
+  const { close } = useResponse();
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,7 +21,7 @@ const Loader = () => {
   }, []);
 
   return (
-    <div className={`bg-black h-screen relative flex items-center justify-center text-[6vw] w-full ${load ? 'top-0 transi' : 'top-[-100%]'}`}>
+    <div className={close ? `bg-black h-screen relative flex items-center justify-center text-[6vw] w-full ${load ? 'top-0 transi' : 'top-[-100%]'}` : `bg-black h-screen relative flex items-center justify-center text-[12vw] w-full ${load ? 'top-0 transi' : 'top-[-100%]'}`}>
       <h1 className="bg-clip-text text-transparent bg-gradient-to-tr from-red-500 to-orange-500">{cont}</h1>
     </div>
 
