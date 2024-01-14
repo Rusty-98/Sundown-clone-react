80e288fc3be78029c1ed673926f267acf4638f1b
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-01-14 10:26:56 +0530

    theek kiya to hai

--- src/Components/Base/Base.jsx
+++ src/Components/Base/Base.jsx
@@ -2,8 +2,8 @@ const Base = ({close}) => {
 
     return (
         <div className={close ? `flex min-h-[650px] ml-10 mr-10 border-b-2 border-gray-500` : `flex flex-col h-[45vh] ml-6 mr-6 border-b-2 border-gray-500`}>
-            <div className={close ? `w-2/5 absolute left-0 bottom-[4%] text-left`:`w-[85%] absolute left-0 bottom-[48%] text-left`}>
-                <h2 className={close ? `text-[38px] pl-10 font-semibold leading-tight`:`text-[24px] w-full pl-10 font-semibold leading-[1]`}>
+            <div className={close ? `w-2/5 absolute left-0 bottom-[4%] text-left`:`w-[85%] absolute left-0 bottom-[46%] text-left`}>
+                <h2 className={close ? `text-[38px] pl-10 font-semibold leading-tight`:`text-[22px] w-full pl-6 pb-3 font-semibold leading-[1]`}>
                     sundown is a multi-disciplinary studio focused on creating unique, end to end experience and eniviroment.
                 </h2>
             </div>
